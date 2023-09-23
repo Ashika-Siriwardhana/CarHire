1349102d4aa8e49dedb80333944115527aa9d5e0
Ashika-Siriwardhan <ashikadhh@gmail.com>
2023-09-23 07:52:24 +0530

    start project

---
+++ src/rent_a_car/Rent_a_Car.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rent_a_car;
+
+/**
+ *
+ * @author user
+ */
+public class Rent_a_Car {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hello");
+    }
+    
+}
